7d5e175b51a07f15ec11a829b11c92c070dee4f2
Riley Farro <rileyfarro@gmail.com>
2021-03-19 16:48:52 +0800

    Refactor some codes and added update function for API

--- app/src/main/java/com/farro/tmdbclient/presentation/App.kt
+++ app/src/main/java/com/farro/tmdbclient/presentation/App.kt
@@ -21,16 +21,12 @@ private lateinit var appComponent:AppComponent
 
     }
 
-    override fun createMovieSubComponent(): MovieSubComponent {
-        return appComponent.movieSubComponent().create()
-    }
+    override fun createMovieSubComponent(): MovieSubComponent =
+        appComponent.movieSubComponent().create()
 
-    override fun createTvShowSubComponent(): TvShowSubComponent {
-       return appComponent.tvShowSubComponent().create()
-    }
+    override fun createTvShowSubComponent(): TvShowSubComponent =
+        appComponent.tvShowSubComponent().create()
 
-    override fun createArtistSubComponent(): ArtistSubComponent {
-        return appComponent.artistSubComponent().create()
-    }
-
+    override fun createArtistSubComponent(): ArtistSubComponent =
+        appComponent.artistSubComponent().create()
 }
